a90beb25f70bd3fba6bee6688f6d65ad27e69449
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-09-18 00:51:31 +0600

    setUp react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import NoMatch from "./Components/NoMatch/NoMatch";
+import UserDetails from "./Components/UserDetails/UserDetails";
+import Users from "./Components/Users/Users";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route path="/userDetails">
+            <UserDetails />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/NoMatch/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div>
+            <p>Nomatch</p>
+        </div>
+    );
+};
+
+export default NoMatch;
